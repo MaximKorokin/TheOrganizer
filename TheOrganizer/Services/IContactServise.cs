3f4428c21489793e6738ce53a87f37af225a21ca
oleksandr.lavrov <oleksandr.lavrov@nure.ua>
2019-04-07 20:26:47 +0300

    ContactsApi

---
+++ TheOrganizer/Services/IContactServise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheOrganizer.Model;
+
+namespace TheOrganizer.Services
+{
+    public interface IContactServise
+    {
+        bool AddContact(Contact contact);
+
+        bool EditContact(Contact contact);
+
+        bool RemoveContact(int comtactId, int ownerId);
+
+        IEnumerable<Contact> GetContacts(int ownerId);
+    }
+}
