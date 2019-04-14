a622a5e16fac3fe81015340820ca927f90a7b08b
Yurii Korokin <yurii.korokin@gmail.com>
2019-04-14 21:24:24 +0300

    Add UserControllerTests.

--- TheOrganizerTests/ControllersTests/UsersControllerTests.cs
+++ TheOrganizerTests/ControllersTests/UsersControllerTests.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TheOrganizer.Controllers;
+using TheOrganizer.Model;
+using TheOrganizer.Services;
+using Xunit;
 
 namespace TheOrganizerTests.ControllersTests
 {
-    class UsersControllerTests
+    public class UsersControllerTests
     {
+        private readonly IUserService _userService;
+
+        public UsersControllerTests(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Fact]
+        public void Authenticate()
+        {
+            var user = new User
+            {
+                Email = "user@example.com",
+                Password = "string",
+            };
+        }
     }
 }
---
+++ TheOrganizerTests/TestServices/TestUserServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheOrganizer.Entities;
+using TheOrganizer.Model;
+using TheOrganizer.Services;
+
+namespace TheOrganizerTests.TestServices
+{
+    public class TestUserServices : IUserService
+    {
+        public bool AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangeUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
