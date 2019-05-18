f66e9c7724d8f59bd8db50e27c8bf572bbaf53c4
Vitamin4ik <38131088+SashaLavrov@users.noreply.github.com>
2019-05-18 17:46:16 +0300

    Displayed events endpoint (#11)
    * Displayed events endpoint
    
    * DisplayedEventsEndpoint
    
    * Limit length of grouping entities names

--- TheOrganizer/Model/Notebook.cs
+++ TheOrganizer/Model/Notebook.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace TheOrganizer.Model
     public class Notebook
     {
         public int Id { get; set; }
+        [StringLength(25)]
         public string Title { get; set; }
         public int OwnerId { get; set; }
 
